934431d4baae93f8c5948636603a06162e45a43e
root <root@dev2.acte.solutions>
2020-02-05 04:55:51 +0000

    v1.0.5

--- routes.php
+++ routes.php
@@ -1,7 +1,3 @@
 <?php
 
-
-Route::get('/api/data', 'Acte\ContentManager\Controllers\Api\Elements@index');
-
-//TEST
-Route::get('/api/fields', 'Acte\ContentManager\Controllers\Api\Elements@getFields');
+  Route::get('/api/data', 'Acte\ContentManager\Controllers\Api\Elements@index');
